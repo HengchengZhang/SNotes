0e1ce16023d2f7ebfdc1a20eb5a644e0b93050a3
HengCheng <79777246+2789372130@users.noreply.github.com>
2023-03-26 15:16:19 +1100

    Docs added

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,5 +1,10 @@
-
+/**
+ * Created HelloWorld object in scala
+ */
 object HelloWorld{
+  /**
+   * The main function that prints Hello World
+   */
   def main(args: Array[String]): Unit = {
     println("Hello World")
   }
