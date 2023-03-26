97053a81d9bcc84c588d953195369076a7005d95
HengCheng <79777246+2789372130@users.noreply.github.com>
2023-03-26 16:28:23 +1100

    Video #3 finished

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -2,9 +2,21 @@
  * Created HelloWorld object in scala
  */
 object HelloWorld{
+
+  val foo:Int = 123
+  val dummy = "123"
+  var age = 23
+  age += 1
+  // String, Int, Char, Boolean, Double, Unit
+  val t = (1, 1.2, "123")
+  // To assign value in t to other variables
+  val (a, b, c) = t
+  // Another way is:
+  println(t._1)
+
   /**
    * The main function that prints Hello World
-   * @param args: The arguments to this application
+   * @param args the arguments to this application
    */
   def main(args: Array[String]): Unit = {
     println("Hello World")
