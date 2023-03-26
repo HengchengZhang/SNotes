359e62d8d1343f29881a100bed1153fced88da50
HengCheng <79777246+2789372130@users.noreply.github.com>
2023-03-26 15:17:12 +1100

    Docs added

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -4,6 +4,7 @@
 object HelloWorld{
   /**
    * The main function that prints Hello World
+   * @param args: The arguments to this application
    */
   def main(args: Array[String]): Unit = {
     println("Hello World")
